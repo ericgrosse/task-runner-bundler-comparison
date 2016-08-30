2674a1cae480fc023ea6e52e44e7c912410a09de
ericgrosse <ericgrosse1@gmail.com>
2016-08-30 05:25:22 -0400

    Modified npm scripts to reflect the removal of webpack-dev-middleware

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -2,16 +2,21 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 const colors = require('colors');
 
+const PROD = process.env.NODE_ENV === 'production';
+const baseDir = PROD ? 'build' : 'dist';
+
 fs.readFile('app/index.html', 'utf8', (err, markup) => {
+  const loadMarkup = cheerio.load(markup);
+  
   if (err) {
     return console.log(err);
   }
 
-  const loadMarkup = cheerio.load(markup);
+  if (PROD) {
+    loadMarkup('head').prepend('<link rel="stylesheet" href="bundle.css">');
+  }
 
-  loadMarkup('head').prepend('<link rel="stylesheet" href="bundle.css">');
-
-  fs.writeFile('build/index.html', loadMarkup.html(), 'utf8', (err) => {
-    err ? console.log(error) : console.log('index.html written to /build'.green);
+  fs.writeFile(baseDir + '/index.html', loadMarkup.html(), 'utf8', (err) => {
+    err ? console.log(error) : console.log(`index.html written to /${baseDir}`.green);
   });
 });
